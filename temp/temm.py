dd0349b95b076ed8b52c3e37b2e675205ace45b7
杨明明 <yangmingming@datatang.com>
2019-07-03 10:37:30 +0800

    '20190703a'

--- temp/temm.py
+++ temp/temm.py
@@ -9,7 +9,7 @@ import re
 from mylib.redis_my import MyRedis
 
 my = MyRedis()
-for i in range(7072):
-    url = my.r.lpop("vietnam_news_thanhnien_content").decode("utf8")
-    if "vtv" not in url:
-        my.r.rpush("vietnam_news_thanhnien_content", url)
+for i in range(20000):
+    url = my.r.lpop("vietnam_news_vtv_content").decode("utf8")
+    if "embed2"not in url:
+        my.r.rpush("vietnam_news_vtv_content", url)
