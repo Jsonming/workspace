331bac4b49e69f25914c10c507220a01db9f7a7e
jsonming <ymmbjcz@126.com>
2019-08-21 15:54:46 +0800

    '20190821'

--- mylib/remove.py
+++ mylib/remove.py
@@ -6,50 +6,48 @@ import time
 import hashlib
 import shutil
 
+
 def getmd5(file):
     if not os.path.isfile(file):
         return
-    fd = open(file,'rb')
+    fd = open(file, 'rb')
     md5 = hashlib.md5()
     md5.update(fd.read())
     fd.close()
     return md5.hexdigest()
 
+
 if __name__ == "__main__":
-    allfile = []
-    md5list = []
-    identicallist = []
+    all_file = []
+    md5_list = []
+    identical_list = []
 
     start = time.time()
     input_path = "E:\\code\\abnormal"
     input_dir = os.listdir(input_path)
     for path in input_dir:
-        inpath=input_path+"\\"+path
-        temp_path = inpath+"\\temp"
+        inpath = input_path + "\\" + path
+        temp_path = inpath + "\\temp"
         uipath = str(inpath)
 
-        for path,dir,filelist in os.walk(uipath):
+        for path, dir, filelist in os.walk(uipath):
             for filename in filelist:
-                pic=os.path.join(path, filename)
-                allfile.append(os.path.join(path,filename))
+                pic = os.path.join(path, filename)
+                all_file.append(os.path.join(path, filename))
 
-    #根据MD5值比较
-    for photo in allfile:
+    # 根据MD5值比较
+    for photo in all_file:
         md5sum = getmd5(photo)
-        if md5sum not in md5list:
-            md5list.append(md5sum)
+        if md5sum not in md5_list:
+            md5_list.append(md5sum)
         else:
-            identicallist.append(photo)
-
+            identical_list.append(photo)
 
     end = time.time()
     last = end - start
-    print(identicallist)
-    for path in identicallist:
-        shutil.move(path,temp_path)
-    print("identical photos: " + str(len(identicallist)))
-    print("time: " + str(last) +"s")
-    print("count: " + str(len(allfile)))
-
-
-
+    print(identical_list)
+    for path in identical_list:
+        shutil.move(path, temp_path)
+    print("identical photos: " + str(len(identical_list)))
+    print("time: " + str(last) + "s")
+    print("count: " + str(len(all_file)))
